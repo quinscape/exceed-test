83371a2d9e81d89041010cea9b8bbdb45aa71ff5
Sven Helmberger <fforw@gmx.de>
2017-11-24 04:22:16 +0100

    Exclude DataSourceAutoConfiguration
    With the new Atomikos data source setup we need to exclude Spring's
    DataSourceAutoConfiguration class.

--- src/main/java/de/quinscape/exceedapp/config/AppConfiguration.java
+++ src/main/java/de/quinscape/exceedapp/config/AppConfiguration.java
@@ -6,13 +6,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {
+    DataSourceAutoConfiguration.class
+})
 @Import({
     ExceedApplicationConfiguration.class,
 })
